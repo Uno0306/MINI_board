bd675581b984e33462676bfa1fe3afd0499b3b8e
Uno0306 <zion6878@naver.com>
2022-07-15 16:58:10 +0900

    Delete console.log

--- my-board/src/context/BoardFunc.js
+++ my-board/src/context/BoardFunc.js
@@ -1,7 +1,6 @@
 import { InsertBoard } from "../context/BoardAxios";
 
 export function handleOnInput(el, maxlength) {
-  console.log(el.value);
   if (el.value.length > maxlength) {
     el.value = el.value.substr(0, maxlength);
   }
